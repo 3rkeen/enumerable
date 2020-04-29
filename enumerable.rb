b1a9b505594a0a9424948b7b4cb8cbcee346c208
Erkin <erkin.sheraliev96@gmail.com>
2020-04-30 01:29:07 +0300

    added #my_each_with_index function

--- enumerable.rb
+++ enumerable.rb
@@ -1,10 +1,20 @@
 module Enumerable
-    def my_each
-        return to_enum :my_each unless block_given?
-      for i in self
-        yield(i)
-    end
-    end
+  def my_each
+      return to_enum :my_each unless block_given?
+    for i in self
+      yield(i)
+  end
   end
 
- p [1, 2, 3, 4, 5].my_each {|i| puts i}
+  def my_each_with_index
+      return to_enum :my_each_with_index unless block_given?
+    index = 0
+      for i in self
+      yield(i, index)
+      index += 1
+  end
+  end
+
+end
+
+["as", "ads", "das"].my_each_with_index {|x, i| puts x if i.even?}
